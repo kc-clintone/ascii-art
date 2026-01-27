ad4729a9238515d7059a8120fc82fa53e76921dd
cclinton <clinton.o.odhiambo@gmail.com>
2026-01-27 16:10:13 +0300

    Add content to main.go to make the program functional

--- main.go
+++ main.go
@@ -1,6 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
+	// handle input
+	if len(os.Args) < 2 {
+		return
+	}
 
+	input := strings.Join(os.Args[1:], " ")
+
+	// load the ascii standard.txt file
+	file_name := "standard.txt"
+	letterMap, err := RetrieveArt(file_name)
+	if err != nil {
+		fmt.Println("Failed to load file: ", err)
+		return
+	}
+
+	// rendering the Art
+	result := AsciiArt(input, letterMap)
+	fmt.Print(result)
 }
