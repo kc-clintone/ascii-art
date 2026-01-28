236bc04e45e149f695a93f7125bb08486c26e678
cclinton <clinton.o.odhiambo@gmail.com>
2026-01-28 14:19:36 +0300

    Added test for the program -- testing with arguments

--- main_test.go
+++ main_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 )
 
+// test for capturing output
 func captureOutput(t *testing.T, fn func()) string {
 	t.Helper()
 
@@ -23,3 +24,10 @@ func captureOutput(t *testing.T, fn func()) string {
 	return buf.String()
 }
 
+// testin gthe program -- argumrnsts provided
+func withArgs(args []string, fn func()) {
+	oldArgs := os.Args
+	defer func() { os.Args = oldArgs }()
+	os.Args = args
+	fn()
+}
