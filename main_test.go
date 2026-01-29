ecb3d8223401b352e4fd9cd846c34b158c33298d
cclinton <clinton.o.odhiambo@gmail.com>
2026-01-29 11:14:37 +0300

    Mod: Optimised the main_test.go

--- main_test.go
+++ main_test.go
@@ -1,66 +1,119 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
 	"strings"
 	"testing"
 )
 
-func generateFont(upTo rune) string {
-	var b strings.Builder
-	for r := ' '; r <= upTo; r++ {
-		for i := range 8 {
-			b.WriteString(string(r))
-			// b.WriteString(byte('0' + i))
-			fmt.Fprintf(&b, "%d", i)
+const (
+	firstChar = ' '
+	lastChar  = '~'
+	height    = 8
+)
 
-			b.WriteString("\n")
+func generateFont(from, to rune) string {
+	var b strings.Builder
+
+	for r := from; r <= to; r++ {
+		for i := 0; i < height; i++ {
+			b.WriteRune(r)
+			b.WriteString(strconv.Itoa(i))
+			b.WriteString(" \n")
 		}
-		b.WriteString("\n")
+		b.WriteByte('\n')
 	}
+
 	return b.String()
 }
 
-func TestMain(t *testing.T) {
+func buildBinary(t *testing.T, dir string) string {
+	bin := filepath.Join(dir, "ascii-art")
+
+	cmd := exec.Command("go", "build", "-o", bin, ".")
+	if out, err := cmd.CombinedOutput(); err != nil {
+		t.Fatalf("build failed: %v\n%s", err, out)
+	}
+
+	return bin
+}
+
+func runProgram(t *testing.T, bin, dir, input string) string {
+	cmd := exec.Command(bin, input)
+	cmd.Dir = dir
+
+	out, err := cmd.Output()
+	if err != nil {
+		t.Fatalf("program failed: %v", err)
+	}
+
+	return string(out)
+}
+
+func expectedSingleChar(r rune) string {
+	var b strings.Builder
+
+	for i := 0; i < height; i++ {
+		b.WriteRune(r)
+		b.WriteString(strconv.Itoa(i))
+		b.WriteString(" \n")
+	}
+
+	return b.String()
+}
+
+func expectedAllChars() string {
+	var b strings.Builder
+
+	for i := 0; i < height; i++ {
+		for r := firstChar; r <= lastChar; r++ {
+			b.WriteRune(r)
+			b.WriteString(strconv.Itoa(i))
+			b.WriteRune(' ')
+		}
+		b.WriteByte('\n')
+	}
+
+	return b.String()
+}
+
+func TestMainExec(t *testing.T) {
 	tmpDir := t.TempDir()
 
-	fontPath := tmpDir + "/standard.txt"
-	if err := os.WriteFile(fontPath, []byte(generateFont('B')), 0644); err != nil {
+	// Write fake font
+	fontPath := filepath.Join(tmpDir, "standard.txt")
+	if err := os.WriteFile(fontPath, []byte(generateFont(firstChar, lastChar)), 0o644); err != nil {
 		t.Fatal(err)
 	}
 
-	oldWd, _ := os.Getwd()
-	defer os.Chdir(oldWd)
-	os.Chdir(tmpDir)
+	// Build program once
+	bin := buildBinary(t, tmpDir)
 
-	origArgs := os.Args
-	os.Args = []string{"ascii-art", "AB"}
-	defer func() { os.Args = origArgs }()
+	t.Run("each character individually", func(t *testing.T) {
+		for r := firstChar; r <= lastChar; r++ {
+			out := runProgram(t, bin, tmpDir, string(r))
+			exp := expectedSingleChar(r)
 
-	origStdout := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
+			if out != exp {
+				t.Fatalf("char %q: expected %q, got %q", r, exp, out)
+			}
+		}
+	})
 
-	main()
+	t.Run("all characters on one line", func(t *testing.T) {
+		var input strings.Builder
+		for r := firstChar; r <= lastChar; r++ {
+			input.WriteRune(r)
+		}
 
-	w.Close()
-	os.Stdout = origStdout
+		out := runProgram(t, bin, tmpDir, input.String())
+		exp := expectedAllChars()
 
-	out, _ := io.ReadAll(r)
-
-		expected := "" +
-		"A0B0\n" +
-		"A1B1\n" +
-		"A2B2\n" +
-		"A3B3\n" +
-		"A4B4\n" +
-		"A5B5\n" +
-		"A6B6\n" +
-		"A7B7\n"
-
-	if string(out) != expected {
-		t.Fatalf("expected %q, got %q", expected, string(out))
-	}
+		if out != exp {
+			t.Fatalf("expected %q, got %q", exp, out)
+		}
+	})
 }
